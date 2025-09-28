1083e10e6596dc833a5bf67b2f66030a9ba452de
Tomi Setsu <xuefubaby@hotmail.com>
2025-09-28 17:56:23 +0900

    Update

--- next.config.js
+++ next.config.js
@@ -4,8 +4,21 @@ const withNextra = nextra({
 
 })
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 module.exports = withNextra({
   output: "export",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   images: {
     unoptimized: true
   }
